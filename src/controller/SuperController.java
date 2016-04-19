a8e6cf568c4a393d60c122160282601fd1d6889d
TeeGreg <gregoire.pommier@etu.univ-rouen.fr>
2016-04-19 15:36:04 +0200

    sc

--- src/controller/SuperController.java
+++ src/controller/SuperController.java
@@ -1,48 +1,81 @@
 package controller;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import javax.swing.JFrame;
 
 import exceptions.network.ServerBadPortException;
+import exceptions.network.ServerSocketAcceptException;
+import gui.BoardDrawer;
+import gui.GraphicBoard;
+import model.board.IBoard;
+import model.board.State;
 import model.coordinates.Coordinates;
 import model.network.Server;
 import model.network.ServerController;
-import model.player.IPlayer;
+import model.player.AIPlayer;
 
 public class SuperController {
 
-    private IPlayer p1;
+    private AIPlayer p1;
     private ServerController sc;
+    private JFrame mainFrame;
+    private boolean turn;
 
 
-
-    public SuperController(String adresse) {
-        createModel();
+    public SuperController(String adresse, Coordinates dimensions, boolean begin) {
+        createModel(dimensions);
         createView();
-        createServerController(adresse);
+        createServerController(adresse, Server.PORT);
+        turn = begin;
     }
     
     private void createView() {
-        // TODO Auto-generated method stub
-        
+        BoardDrawer<State> pinceau = new BoardDrawer<State>() {
+            @Override
+            public void drawOnBoard(Graphics g, IBoard<State> board,
+                    Coordinates axes, float scale) {
+            }
+        };
+       mainFrame = new JFrame();
+       mainFrame.setPreferredSize(new Dimension(500, 500));
+       mainFrame.add(new GraphicBoard<State>(p1.getShootGrid(), new Coordinates(-1, -1, 1), pinceau), BorderLayout.CENTER);
+       mainFrame.pack();
+       mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       mainFrame.setLocationRelativeTo(null);
+       mainFrame.setVisible(true);
     }
 
-    private void createModel() {
-        
+    private void createModel(Coordinates dimensions) {
+        p1 = new AIPlayer(dimensions);
     }
 
 
     private void createServerController(String adresse, int port) {
         try {
-            this.sc = new ServerController(this.p1, new Server(port == 0 ? port : Server.PORT, Server.BACKLOG, InetAddress.getByName(adresse)));
+            try {
+                this.sc = new ServerController(this.p1, new Server(port == 0 ? port : Server.PORT, Server.BACKLOG, InetAddress.getByName(adresse)));
+            } catch (ServerSocketAcceptException | UnknownHostException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         } catch (ServerBadPortException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
+    
+    public void startGame() {
+        if (turn) {
+        sc.setData(p1.shoot().toString());
+        sc.sendData();
+    }
 
 
 
-    public void startGameSinglePlayer(Coordinates dimension) {
-        
     }
 }
